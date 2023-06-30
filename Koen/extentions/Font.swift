a3a5ab352df98376ad091888e3576da2af64191c
Guen <heloo@guen.dev>
2023-06-30 08:35:35 +0700

    cm

---
+++ Koen/extentions/Font.swift
@@ -0,0 +1,15 @@
+//
+//  Font.swift
+//  Koen
+//
+//  Created by Guen on 27/06/2023.
+//
+
+import SwiftUI
+
+
+extension Font {    
+    public static func kumbh(size: CGFloat = 16) -> Font {
+        self.custom("Kumbh Sans", size: size)
+    }
+}
